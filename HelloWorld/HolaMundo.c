2341d38c078dd96f068b6145f70ef3ff11d7fd5e
Raziel Guendulain <octavioraziel.gdn@gmail.com>
2023-02-12 14:52:16 -0600

    First commit for the online repo

---
+++ HelloWorld/HolaMundo.c
@@ -0,0 +1,20 @@
+//Headers - Encabezados
+#include <stdio.h>
+
+// Macros 
+// Global variables - Variables Globales
+// Function prototypes - prototipos de función
+// Main function - Función principal
+
+int main (){
+	printf ("Hola mundo\n");
+	return 0;
+}
+// Functions - Funciones 
+
+
+
+
+
+
+
