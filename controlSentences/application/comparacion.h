1f08827e7bd70dc469e0a019b6867ea6f465e636
Raziel Guendulain <octavioraziel.gdn@gmail.com>
2023-05-05 19:52:27 -0600

    Update comparacion.h f

--- controlSentences/application/comparacion.h
+++ controlSentences/application/comparacion.h
@@ -1,2 +1,6 @@
 
 int numeroMayor(int primero, int segundo);
+int MayorDeTres(int numA, int numB, int numC);
+int numeroMenor(int primero, int segundo);
+int menorDeTres(int numA, int numB, int numC);
+int comparacionDeNumeros(int numA, int numB);
