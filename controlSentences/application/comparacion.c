2598910a786dca5d16d5b205627860dd78811e24
Raziel Guendulain <octavioraziel.gdn@gmail.com>
2023-05-05 19:51:37 -0600

    Agregadas comparaciones menores

--- controlSentences/application/comparacion.c
+++ controlSentences/application/comparacion.c
@@ -2,35 +2,80 @@
  * La funcion mayor retorna el mayor de dos números
  **/
 
-int numeroMayor(int primero, int segundo){
-    int mayor=0;
+int numeroMayor(int primero, int segundo)
+{
+    int mayor = 0;
     
     if (primero > segundo)
     {
-        mayor=primero;
+        mayor = primero;
     } 
     else
     {
-        mayor=segundo;
+        mayor = segundo;
     }
     return mayor;
     
 }
-int MayorDeTres(int numA, int numB, int numC){
+int MayorDeTres(int numA, int numB, int numC)
+{
     int mayor=0;
-    if (numA >numB ){
+    if (numA > numB ){
         mayor=numA;
     }
     else 
     {
         if (numB > numC)
         {    
-            mayor= numB; 
+            mayor = numB; 
         }
         else
         {   
-            mayor=numC;
+            mayor = numC;
         }
     }
     return mayor;   
+}
+
+int numeroMenor(int primero, int segundo)
+{
+    int menor=0;
+    
+    if (primero < segundo)
+    {
+        menor=primero;
+    } 
+    else
+    {
+        menor=segundo;
+    }
+    return menor;
+    
+}
+int menorDeTres(int numA, int numB, int numC)
+{
+    int menor=0;
+    if (numA < numB ){
+        menor=numA;
+    }
+    else 
+    {
+        if (numB < numC)
+        {    
+            menor= numB; 
+        }
+        else
+        {   
+            menor=numC;
+        }
+    }
+    return menor;   
+}
+/** Esta funcion toma como parámetros dos números enteros y regresa un número positivo si el primer numero (numA)
+ * es mayor que el segundo (numB), asi mismo regresa un 0 si son iguales y un número negativo si es menor. 
+ * En la práctica es la mejor forma de comparación cuando se utilizan algoritmos de ordenación. 
+*/
+int comparacionDeNumeros(int numA, int numB)
+{
+    return numA - numB;
 }
