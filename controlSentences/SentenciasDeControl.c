17a8957451fa1c16caf80430cff69cdaceb7a748
Octavio Raziel Güendulain Pérez <RazielGDN>
2023-06-05 23:06:15 -0600

    Changes in SentenciasDeControl

--- controlSentences/SentenciasDeControl.c
+++ controlSentences/SentenciasDeControl.c
@@ -8,19 +8,19 @@ int main (){
     int temporal=0;
     /*Prueba de función que calcula el mayor de dos números*/
     temporal=numeroMayor(numero1, numero2);
-    printf("El mayor de a=%d y b=%d es %d\n ", numero1,numero2,temporal);
+    printf("El mayor de a=%d y b=%d es %d\n", numero1,numero2,temporal);
     /*Prueba de función que calcula mayor de tres números */
     temporal=mayorDeTres(numero1,numero2,numero3);
-    printf("El mayor de a=%d,  b=%d y c=%d es %d\n ", numero1,numero2,numero3,temporal);   
+    printf("El mayor de a=%d,  b=%d y c=%d es %d\n", numero1,numero2,numero3,temporal);   
 
     /*Prueba de función que calcula el menor de dos números*/
     temporal=numeroMenor(numero1, numero2);
-    printf("El mayor de a=%d y b=%d es %d\n ", numero1,numero2,temporal);
+    printf("El menor de a=%d y b=%d es %d\n", numero1,numero2,temporal);
     /*Prueba de función que calcula menor de tres números */
     temporal=menorDeTres(numero1,numero2,numero3);
-    printf("El mayor de a=%d,  b=%d y c=%d es %d\n ", numero1,numero2,numero3,temporal);   
+    printf("El menor de a=%d,  b=%d y c=%d es %d\n", numero1,numero2,numero3,temporal);   
     
-    comparacionDeNumeros(numero1, numero2);
+    //comparacionDeNumeros(numero1, numero2);
     return 0;
 }
 
